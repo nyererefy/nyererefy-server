6ffce2bcb50f8e57ee14cd0ed8644b17c7f29683
Sylvanus Kateile <kateile@outlook.com>
2019-11-16 09:45:01 +0300

    make schoolPrograms nullable in school

--- src/entities/school.ts
+++ src/entities/school.ts
@@ -32,9 +32,9 @@ export class School {
      * Why ManyToOne?
      * -Because School of Pharmacy offers two schoolPrograms BPHARM and DPS
      */
-    @Field(() => [SchoolProgram])
+    @Field(() => [SchoolProgram], {nullable: true})
     @OneToMany(() => SchoolProgram, s => s.school, {eager: true})
-    schoolPrograms: SchoolProgram[];
+    schoolPrograms: SchoolProgram[]; //todo make this nullable too!!!!!
 
     /* ManyToOne */
     @ManyToOne(() => Branch, s => s.schools)
